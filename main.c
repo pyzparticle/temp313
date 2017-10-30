3a9ae0b9ef19ef41c29a927849e3910afe8feefe
Patrick Zhao <pzhao@trustlook.com>
2017-10-30 12:56:35 -0700

    Formatting changes

--- main.c
+++ main.c
@@ -2,77 +2,77 @@
 
 static void flush(int dim)
 {
-    const int size = FLUSH_AMT;
-    char *c = (char *)malloc(size);
-    for (int i = 0; i < 0xf; i++)
-        for (int j = 0; j < size; j++)
-            c[j] = i+j;
+        const int size = FLUSH_AMT;
+        char *c = (char *)malloc(size);
+        for (int i = 0; i < 0xf; i++)
+                for (int j = 0; j < size; j++)
+                        c[j] = i+j;
 }
 
 int main()
 {
-    clock_t result, low;
-    int i, j, t_size, size, best;
+        clock_t result, low;
+        int i, j, t_size, size, best;
 
-    // Allocate memory for A 1024x1024
-    size = ROW * sizeof(int *) + ROW * COL * sizeof(int);
-    int **A =  malloc(size);
-    //printf("Allocate memory for A 1024*1024 (size = %d Byte). \n", size);
-    int *offsA = &A[ROW];
-    for(i = 0; i < ROW; i++, offsA+= COL){
-        A[i] = offsA;
-    }
+        // Allocate memory for A 1024x1024
+        size = ROW * sizeof(int *) + ROW * COL * sizeof(int);
+        int **A =  malloc(size);
+        //printf("Allocate memory for A 1024*1024 (size = %d Byte). \n", size);
+        int *offsA = &A[ROW];
+        for(i = 0; i < ROW; i++, offsA+= COL) {
+                A[i] = offsA;
+        }
 
-    // Allocate memory for B 1024 x (1024+3))
-    size = ROW * sizeof(int *) + ROW * (COL+3) * sizeof(int);
-    int **B =  malloc(size);
-    //printf("Allocate memory for B 1024*1027 (size = %d Byte). \n", size);
-    int *offsB = &B[ROW];
-    for(i = 0; i < ROW; i++, offsB+= (COL+3)){
-        B[i] = offsB;
-    }
+        // Allocate memory for B 1024 x (1024+3))
+        size = ROW * sizeof(int *) + ROW * (COL+3) * sizeof(int);
+        int **B =  malloc(size);
+        //printf("Allocate memory for B 1024*1027 (size = %d Byte). \n", size);
+        int *offsB = &B[ROW];
+        for(i = 0; i < ROW; i++, offsB+= (COL+3)) {
+                B[i] = offsB;
+        }
 
-    // Allocate memory for C 1024x(1204+5)
-    size = ROW * sizeof(int *) + ROW * (COL+5) * sizeof(int);
-    int **C =  malloc(ROW * sizeof(int *) + ROW * (COL+5) * sizeof(int));
-    //printf("Allocate memory for C 1024*1029 (size = %d Byte). \n\n", size);
-    int *offsC = &C[ROW];
-    for(i = 0; i < ROW; i++, offsC+= (COL+5)){
-        C[i] = offsC;
-    }
+        // Allocate memory for C 1024x(1204+5)
+        size = ROW * sizeof(int *) + ROW * (COL+5) * sizeof(int);
+        int **C =  malloc(ROW * sizeof(int *) + ROW * (COL+5) * sizeof(int));
+        //printf("Allocate memory for C 1024*1029 (size = %d Byte). \n\n", size);
+        int *offsC = &C[ROW];
+        for(i = 0; i < ROW; i++, offsC+= (COL+5)) {
+                C[i] = offsC;
+        }
 
-    // A, B assign random value
-    initial_matrix(A, ROW, COL, "A");
-    initial_matrix(B, ROW, COL, "B");
+        // A, B assign random value
+        initial_matrix(A, ROW, COL, "A");
+        initial_matrix(B, ROW, COL, "B");
 
-    low = 0xFFFFFFFF;
-    best = 0;
-    printf("square: ");
-    for (t_size = MIN_TILE; t_size <= MAX_TILE; t_size = t_size << 1) {
-        clear_matrix(C);
-        flush(FLUSH_AMT);
-        result =  matrix_multiplication(C, A, B, t_size);
-        if (result < low){
-            low = result;
-	    best = t_size;
-	}	
-        if (VERBOSE == 0)
-            printf("%lu,", result);
-    }
-    printf("best:%d-%lu\n", best, low);
+        low = 0xFFFFFFFF;
+        best = 0;
+        printf("square: ");
+        for (t_size = MIN_TILE; t_size <= MAX_TILE; t_size = t_size << 1) {
+                clear_matrix(C);
+                flush(FLUSH_AMT);
+                result =  matrix_multiplication(C, A, B, t_size);
+                if (result < low) {
+                        low = result;
+                        best = t_size;
+                }
+                if (VERBOSE == 0)
+                        printf("%lu,", result);
+        }
+        printf("best:%d-%lu\n", best, low);
 
-    low = 0xFFFFFFFF;
-    printf("non-sq: ");
-    for (t_size = best - 4; t_size < best + 4; t_size++ ) {
-        clear_matrix(C);
-        flush(FLUSH_AMT);
-        result =  matrix_mult_uneven(C, A, B, t_size);
-	if (result < low){
-            low = result;
-	    best = t_size;
-	}
-        if (VERBOSE == 0)
-            printf("%lu,", result);
-    }
-    printf("best:%d-%lu\n", best, low);
+        low = 0xFFFFFFFF;
+        printf("non-sq: ");
+        for (t_size = best - 4; t_size < best + 4; t_size++ ) {
+                clear_matrix(C);
+                flush(FLUSH_AMT);
+                result =  matrix_mult_uneven(C, A, B, t_size);
+                if (result < low) {
+                        low = result;
+                        best = t_size;
+                }
+                if (VERBOSE == 0)
+                        printf("%lu,", result);
+        }
+        printf("best:%d-%lu\n", best, low);
 }
